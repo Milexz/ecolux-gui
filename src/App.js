3bf46fd7f28c3e5a0f58efd47fbae4e48de2d828
Max CHEN <Max_Chen@alphanetworks.com>
2023-10-19 03:01:01 +0000

    feat: add react router dom for route pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 
+import React from 'react';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+import NotFound from './Pages/NotFound';
+
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Simple React App for ECOLUX</p>
       </header>
       <main className="App-main">
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/login" />
+            </Route>
+            <Route path="/login" component={Login} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </BrowserRouter>
       </main>
       <footer className="App-footer">
         <p>By Max Chen</p>
