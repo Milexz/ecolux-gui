c209b72958f37c72a306c73f15e08e975c9d167c
Max CHEN <Max_Chen@alphanetworks.com>
2023-10-19 04:24:00 +0000

    feat: implement login functions

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,6 +1,15 @@
 import React from 'react';
 
 export default function Dashboard() {
+  const loginSuccess = sessionStorage.getItem('loginSuccess');
+  
+  if (loginSuccess !== 'true') {
+    window.location.href = '/login';
+    return (
+      <h3>Checking Authority...</h3>
+    );
+  }
+
   return (
     <div>Dashboard</div>
   );
--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,7 +1,132 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    justifyContent: 'center',
+    margin: '100px auto',
+    height: '300px',
+    width: '300px',
+  },
+  '@media (max-width: 600px)': {
+    root: {
+      height: '300px',
+      width: '200px',
+    },
+  },
+}));
 
 export default function Login() {
+  const [email, setEmail] = React.useState('');
+  const [emailError, setEmailError] = React.useState(false);
+  const [emailHelperText, setEmailHelperText] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [passwordError, setPasswordError] = React.useState(false);
+  const [passwordHelperText, setPasswordHelperText] = React.useState('');
+  const classes = useStyles();
+
+  const loginSuccess = sessionStorage.getItem('loginSuccess');
+  const emailRegex = '^[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$';
+  const passwordRegex = '^[a-zA-Z0-9]+$';
+
+  if (loginSuccess === 'true') {
+    window.location.href = '/dashboard';
+    return (
+      <h3>Checking Authority...</h3>
+    );
+  }
+
+  const checkEmailFormat = (email) => {
+    console.log('checkEmailFormat: ', email);
+    if (email.trim().length === 0) {
+      setEmailError(true);
+      setEmailHelperText('Email is required');
+      return false;
+    }
+    if (email.match(emailRegex) === null) {
+      setEmailError(true);
+      setEmailHelperText('Email format error');
+      return false;
+    }
+    
+    setEmailError(false);
+    setEmailHelperText('');
+    return true;
+  };
+
+  const checkPasswordFormat = (password) => {
+    console.log('checkPasswordFormat: ', password);
+    if (password.trim().length === 0) {
+      setPasswordError(true);
+      setPasswordHelperText('Password is required');
+      return false;
+    }
+    if (password.match(passwordRegex) === null) {
+      setPasswordError(true);
+      setPasswordHelperText('Password format error: only letters and numbers are allowed');
+      return false;
+    }
+    
+    setPasswordError(false);
+    setPasswordHelperText('');
+    return true;
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log('Login');
+    const validEmail = checkEmailFormat(email);
+    const validPassword = checkPasswordFormat(password);
+
+    if (validEmail && validPassword) {
+      sessionStorage.setItem('loginSuccess', 'true');
+      window.location.href = '/dashboard';
+    }
+  };
+
+  const handleEmailChange = (event) => {
+    console.log('Email: ', event.target.value);
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    console.log('Password: ', event.target.value);
+    setPassword(event.target.value);
+  };
+
   return (
-    <div>Login</div>
+    <div className={classes.root}>
+      <h2>Welcome!</h2>
+      <p>Login by entering email and password.</p>
+      <form className={classes.form} autoComplete="on" onSubmit={handleLogin}>
+        <TextField id="login-email" className={classes.textField}
+          label="Email"
+          name="email"
+          onChange={handleEmailChange}
+          // required
+          // type="email"
+          value={email}
+          error={emailError}
+          helperText={emailHelperText}
+          variant="outlined"
+          color="primary"
+        />
+        <TextField id="login-password" className={classes.textField}
+          label="Password"
+          name="password"
+          onChange={handlePasswordChange}
+          // required
+          type="password"
+          value={password}
+          error={passwordError}
+          helperText={passwordHelperText}
+          variant="outlined"
+          color="primary"
+        />
+        <Button id="login-sign-in-button" className={classes.submitButton} type="submit" variant="contained" color="primary">Login</Button>
+      </form>
+    </div>
   );
 }
