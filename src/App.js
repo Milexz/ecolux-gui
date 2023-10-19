d005f93edef4c6189beac35e923dc71d23d6da88
Max CHEN <Max_Chen@alphanetworks.com>
2023-10-19 02:08:38 +0000

    style: template layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Simple React App for ECOLUX</p>
       </header>
+      <main className="App-main">
+      </main>
+      <footer className="App-footer">
+        <p>By Max Chen</p>
+      </footer>
     </div>
   );
 }
