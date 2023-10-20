3df18fc9807d20df305224ce86d0b02d358970be
Max CHEN <Max_Chen@alphanetworks.com>
2023-10-20 02:31:47 +0000

    style: dashboard layout

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -14,34 +14,26 @@ import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 const useStyles = makeStyles({
   root: {
     display: 'flex',
-    flexDirection: 'row',
+    alignItems: 'center',
+    flexDirection: 'column',
+    minWidth: '375px',
   },
-  table: {
-    width: '48%',
+  tableArea: {
+    width: '60%',
     maxWidth: '800px',
+    minWidth: '360px',
     padding: '1%',
+    marginBottom: '24px',
   },
-  dataTable: {
-    margin: '0 auto',
+  barChartArea: {
+    position: 'relative',
+    width: '60%',
     maxWidth: '800px',
-    minWidth: '400px',
-  },
-  barChart: {
-    width: '48%',
+    minWidth: '360px',
+    height: '320px',
     padding: '1%',
-  },
-  '@media (max-width: 960px)': {
-    root: {
-      // display: 'unset',
-      flexDirection: 'column',
-    },
-    table: {
-      width: '96%',
-      padding: '2%',
-    },
-    barChart: {
-      width: '96%',
-      padding: '2%',
+    '& > :not(:last-child)': {
+      marginBottom: '40px',
     },
   },
 });
@@ -51,8 +43,8 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Yoghurt', 159, 6.0, 24, 4.0),
+  createData('Sandwich', 237, 9.0, 37, 4.3),
   createData('Eclair', 262, 16.0, 24, 6.0),
   createData('Cupcake', 305, 3.7, 67, 4.3),
   createData('Gingerbread', 356, 16.0, 49, 3.9),
@@ -72,8 +64,7 @@ export default function Dashboard() {
 
   return (
     <div className={classes.root}>
-      <div className={classes.table}>
-        Data Table
+      <div className={classes.tableArea}>
         <TableContainer component={Paper}>
           <Table className={classes.dataTable}>
             <TableHead>
@@ -99,17 +90,14 @@ export default function Dashboard() {
           </Table>
         </TableContainer>
       </div>
-      <div className={classes.barChart}>
-        Bar Chart
-        {/* <ResponsiveContainer width="100%" height="100%"> */}
+      <div className={classes.barChartArea}>
+        <ResponsiveContainer width="100%" height="100%">
           <BarChart
-            width={800}
-            height={300}
             data={rows}
             margin={{
               top: 5,
-              right: 30,
-              left: 20,
+              right: 25,
+              left: 5,
               bottom: 5,
             }}
           >
@@ -118,10 +106,29 @@ export default function Dashboard() {
             <YAxis />
             <Tooltip />
             <Legend />
-            <Bar dataKey="calories" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
-            <Bar dataKey="fat" fill="#82ca9d" activeBar={<Rectangle fill="gold" stroke="purple" />} />
+            <Bar dataKey="calories" fill="#8884d8" maxBarSize={50} activeBar={<Rectangle fill="pink" stroke="purple" />} />
           </BarChart>
-        {/* </ResponsiveContainer> */}
+        </ResponsiveContainer>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            data={rows}
+            margin={{
+              top: 5,
+              right: 25,
+              left: 5,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="fat" stackId="a" fill="#82ca9d"  maxBarSize={50} />
+            <Bar dataKey="carbs" stackId="a" fill="#ffc658"  maxBarSize={50} />
+            <Bar dataKey="protein" stackId="a" fill="#9ac2d8"  maxBarSize={50} />
+          </BarChart>
+        </ResponsiveContainer>
       </div>
       
     </div>
